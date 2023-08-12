ea22c5c5d99639231f0620737085f6980b84b59a
Henrry Bourgeot <henrrybrgt@gmail.com>
2023-08-12 01:17:44 -0400

    adding telegram getUpdates struct and mongodb connection

--- main.go
+++ main.go
@@ -1,15 +1,63 @@
 package main
 
 import (
+	"context"
 	"encoding/json"
+	"fmt"
+	"log"
 	"net/http"
 	"time"
+
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
-var client *http.Client
+// global variables
+var (
+	httpClient *http.Client
+	collection *mongo.Collection
+)
+
+// constants
+const (
+	connectionString = "mongodb://localhost:27017/"
+	dbName           = "gokarmas"
+	colName          = "karmas"
+)
+
+func init() {
+	// client options
+	clientOptions := options.Client().ApplyURI(connectionString)
+
+	// connect to mongodb
+	client, err := mongo.Connect(context.TODO(), clientOptions)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Mongonnected")
+
+	collection = client.Database(dbName).Collection(colName)
+
+	// collection instance
+	fmt.Println("Mongollection is ready")
+}
+
+func GetUpdate() {
+	url := "https://api.telegram.org/bot5917051686:AAEf5hPR7stgvKb2Ig38IXfydEk88vpnUnI/getUpdates"
+
+	var update Updates
+
+	if err := GetJson(url, &update); err != nil {
+		fmt.Printf("error getting update: %s\n", err.Error())
+		return
+	} else {
+		fmt.Printf("Update received!")
+	}
+}
 
 func GetJson(url string, target interface{}) error {
-	resp, err := client.Get(url)
+	resp, err := httpClient.Get(url)
 	if err != nil {
 		return err
 	}
@@ -20,5 +68,7 @@ func GetJson(url string, target interface{}) error {
 }
 
 func main() {
-	client = &http.Client{Timeout: 10 * time.Second}
+	httpClient = &http.Client{Timeout: 10 * time.Second}
+
+	GetUpdate()
 }
---
+++ structs.go
@@ -0,0 +1,147 @@
+package main
+
+type Updates struct {
+	Ok     bool     `json:"ok"`
+	Result []Result `json:"result"`
+}
+
+type Result struct {
+	EditedMessage EditedMessage `json:"edited_message"`
+	Message       Message       `json:"message"`
+	UpdateID      int64         `json:"update_id"`
+}
+
+type EditedMessage struct {
+	Chat            Chat                        `json:"chat"`
+	Date            int64                       `json:"date"`
+	EditDate        int64                       `json:"edit_date"`
+	From            From                        `json:"from"`
+	IsTopicMessage  bool                        `json:"is_topic_message"`
+	MessageID       int64                       `json:"message_id"`
+	MessageThreadID *int64                      `json:"message_thread_id"`
+	ReplyToMessage  EditedMessageReplyToMessage `json:"reply_to_message"`
+	Text            string                      `json:"text"`
+}
+
+type Chat struct {
+	ID       int64  `json:"id"`
+	IsForum  bool   `json:"is_forum"`
+	Title    string `json:"title"`
+	Type     string `json:"type"`
+	Username string `json:"username"`
+}
+
+type From struct {
+	FirstName    string `json:"first_name"`
+	ID           int64  `json:"id"`
+	IsBot        bool   `json:"is_bot"`
+	LanguageCode string `json:"language_code"`
+	LastName     string `json:"last_name"`
+	Username     string `json:"username"`
+}
+
+type EditedMessageReplyToMessage struct {
+	Chat            Chat   `json:"chat"`
+	Date            int64  `json:"date"`
+	EditDate        int64  `json:"edit_date"`
+	From            From   `json:"from"`
+	IsTopicMessage  bool   `json:"is_topic_message"`
+	MessageID       int64  `json:"message_id"`
+	MessageThreadID int64  `json:"message_thread_id"`
+	Text            string `json:"text"`
+}
+
+type Message struct {
+	Animation          Animation             `json:"animation"`
+	Caption            string                `json:"caption"`
+	CaptionEntities    []Entity              `json:"caption_entities"`
+	Chat               Chat                  `json:"chat"`
+	Date               int64                 `json:"date"`
+	Document           Document              `json:"document"`
+	Entities           []Entity              `json:"entities"`
+	From               From                  `json:"from"`
+	IsTopicMessage     bool                  `json:"is_topic_message"`
+	MessageID          int64                 `json:"message_id"`
+	MessageThreadID    *int64                `json:"message_thread_id"`
+	NewChatMember      From                  `json:"new_chat_member"`
+	NewChatMembers     []From                `json:"new_chat_members"`
+	NewChatParticipant From                  `json:"new_chat_participant"`
+	Photo              []Thumb               `json:"photo"`
+	ReplyToMessage     MessageReplyToMessage `json:"reply_to_message"`
+	Sticker            Sticker               `json:"sticker"`
+	Text               string                `json:"text"`
+}
+
+type Animation struct {
+	Duration     int64  `json:"duration"`
+	FileID       string `json:"file_id"`
+	FileName     string `json:"file_name"`
+	FileSize     int64  `json:"file_size"`
+	FileUniqueID string `json:"file_unique_id"`
+	Height       int64  `json:"height"`
+	MIMEType     string `json:"mime_type"`
+	Thumb        Thumb  `json:"thumb"`
+	Thumbnail    Thumb  `json:"thumbnail"`
+	Width        int64  `json:"width"`
+}
+
+type Thumb struct {
+	FileID       string `json:"file_id"`
+	FileSize     int64  `json:"file_size"`
+	FileUniqueID string `json:"file_unique_id"`
+	Height       int64  `json:"height"`
+	Width        int64  `json:"width"`
+}
+
+type Entity struct {
+	Length int64  `json:"length"`
+	Offset int64  `json:"offset"`
+	Type   string `json:"type"`
+}
+
+type Document struct {
+	FileID       string `json:"file_id"`
+	FileName     string `json:"file_name"`
+	FileSize     int64  `json:"file_size"`
+	FileUniqueID string `json:"file_unique_id"`
+	MIMEType     string `json:"mime_type"`
+	Thumb        Thumb  `json:"thumb"`
+	Thumbnail    Thumb  `json:"thumbnail"`
+}
+
+type MessageReplyToMessage struct {
+	Caption           string            `json:"caption"`
+	CaptionEntities   []Entity          `json:"caption_entities"`
+	Chat              Chat              `json:"chat"`
+	Date              int64             `json:"date"`
+	EditDate          *int64            `json:"edit_date"`
+	ForumTopicCreated ForumTopicCreated `json:"forum_topic_created"`
+	From              From              `json:"from"`
+	IsTopicMessage    bool              `json:"is_topic_message"`
+	MessageID         int64             `json:"message_id"`
+	MessageThreadID   *int64            `json:"message_thread_id"`
+	Photo             []Thumb           `json:"photo"`
+	Sticker           Sticker           `json:"sticker"`
+	Text              string            `json:"text"`
+}
+
+type Sticker struct {
+	Emoji        string `json:"emoji"`
+	FileID       string `json:"file_id"`
+	FileSize     int64  `json:"file_size"`
+	FileUniqueID string `json:"file_unique_id"`
+	Height       int64  `json:"height"`
+	IsAnimated   bool   `json:"is_animated"`
+	IsVideo      bool   `json:"is_video"`
+	SetName      string `json:"set_name"`
+	Thumb        Thumb  `json:"thumb"`
+	Thumbnail    Thumb  `json:"thumbnail"`
+	Type         string `json:"type"`
+	Width        int64  `json:"width"`
+}
+
+type ForumTopicCreated struct {
+	IconColor         int64  `json:"icon_color"`
+	IconCustomEmojiID string `json:"icon_custom_emoji_id"`
+	Name              string `json:"name"`
+}
